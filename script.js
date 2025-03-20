48acdc21242748a1a740b9773922c091f1a7f7f7
ayomide <olorunfemiayomide045@gmail.com>
2025-03-20 22:29:31 +0100

    commit all

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function toggleMenu() {
     const navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("active");
 }
+
+let index = 0;
+
+function moveCarousel() {
+    const carouselContainer = document.querySelector(".carousel-container");
+    const images = document.querySelectorAll(".carousel-image");
+    index++;
+
+    if (index >= images.length) {
+        index = 0;
+    }
+
+    const offset = -index * 100;
+    carouselContainer.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(moveCarousel, 3000); 
